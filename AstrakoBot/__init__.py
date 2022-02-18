9768d6e4c38d1a03ca8f3fd4dd162513c7fb0dcd
Lil Kennedy 炎 <87056837+kennedy-ex@users.noreply.github.com>
2022-02-18 14:30:14 +0700

    gitu doang

--- AstrakoBot/__init__.py
+++ AstrakoBot/__init__.py
@@ -65,6 +65,7 @@ if ENV:
     API_ID = os.environ.get("API_ID", None)
     API_HASH = os.environ.get("API_HASH", None)
     DB_URI = os.environ.get("DATABASE_URL")
+    DB_URI = DB_URI.replace("postgres://", "postgresql://", 1)
     DONATION_LINK = os.environ.get("DONATION_LINK")
     LOAD = os.environ.get("LOAD", "").split()
     NO_LOAD = os.environ.get("NO_LOAD", "translation").split()
